53307b4a08a29c7a0d7985f92f91fc01ecf259d7
Rohith <gambol99@gmail.com>
2020-02-13 00:06:49 +0000

    Admin Token
    - removing the admin token from the resource - was never used

--- pkg/apis/clusters/v1/kubernetes_types.go
+++ pkg/apis/clusters/v1/kubernetes_types.go
@@ -20,7 +20,6 @@ package v1
 import (
 	corev1 "github.com/appvia/kore/pkg/apis/core/v1"
 
-	core "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -79,8 +78,6 @@ type ClusterUser struct {
 // KubernetesStatus defines the observed state of Cluster
 // +k8s:openapi-gen=true
 type KubernetesStatus struct {
-	// AdminToken is the kore-admin service account token which is bound to cluster-admin
-	AdminToken core.Secret `json:"adminToken,omitempty"`
 	// CaCertificate is the base64 encoded cluster certificate
 	CaCertificate string `json:"caCertificate,omitempty"`
 	// Components is a collection of component statuses
@@ -91,7 +88,7 @@ type KubernetesStatus struct {
 	// Endpoint is the kubernetes endpoint url
 	APIEndpoint string `json:"apiEndpoint,omitempty"`
 	// Status is overall status of the workspace
-	Status corev1.Status `json:"status"`
+	Status corev1.Status `json:"status,omitempty"`
 }
 
 // +genclient
