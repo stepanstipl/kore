2b6c381aedec2594cb67f0a7a2d10bc4df4832a6
bandesz <bandesz@gmail.com>
2020-03-04 14:01:10 +0000

    Fix copyright notice

--- pkg/clusterappman/manifests.go
+++ pkg/clusterappman/manifests.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Lewis Marsahll <lewis.marshall@appvia.io>
+ * Copyright (C) 2019 Appvia Ltd <info@appvia.io>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
