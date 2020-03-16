38012b884efdc3e8fdc9c306864652fc58f14bd9
Lewis Marshall <lewis.marshall@appvia.io>
2020-03-16 15:44:42 +0000

    small api fixups

--- pkg/apis/eks/v1alpha1/eksnodegroup_types.go
+++ pkg/apis/eks/v1alpha1/eksnodegroup_types.go
@@ -25,20 +25,19 @@ import (
 // EKSNodeGroupSpec defines the desired state of EKSNodeGroup
 // +k8s:openapi-gen=true
 type EKSNodeGroupSpec struct {
-	AMIType string `json:"aMIType,omitempty"`
+	AMIType string `json:"amiType"`
 	// +kubebuilder:validation:Required
-	ClusterName string `json:"clusterName"`
-	DiskSize    int64  `json:"diskSize,omitempty"`
-	// +listType=set
-	InstanceTypes []string          `json:"instanceTypes,omitempty"`
+	ClusterName   string            `json:"clusterName"`
+	DiskSize      int64             `json:"diskSize,omitempty"`
+	InstanceType  string            `json:"instanceType,omitempty"`
 	Labels        map[string]string `json:"labels,omitempty"`
 	NodeGroupName string            `json:"nodeGroupName"`
 	// +kubebuilder:validation:Required
-	NodeRole       string `json:"nodeRole"`
+	IamNodeRole    string `json:"iamNodeRole"`
 	ReleaseVersion string `json:"releaseVersion,omitempty"`
 	RemoteAccess   string `json:"remoteAccess,omitempty"`
 	DesiredSize    int64  `json:"desiredSize,omitempty"`
-	// +kubebuilder:validation:Minimum=100
+	// +kubebuilder:validation:Maximum=100
 	MaxSize int64 `json:"maxSize,omitempty"`
 	// +kubebuilder:validation:Minimum=0
 	MinSize int64 `json:"minSize,omitempty"`
@@ -51,13 +50,13 @@ type EKSNodeGroupSpec struct {
 	Version string `json:"version,omitempty"`
 	// AWS region to launch node group within, must match the region of the cluster
 	Region string `json:"region"`
+	// The security groups that are allowed SSH access (port 22) to the worker nodes
+	SSHSourceSecurityGroups []string `json:"sshSourceSecurityGroups,omitempty"`
 	// The Amazon EC2 SSH key that provides access for SSH communication with
 	// the worker nodes in the managed node group
 	// https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html
 	// +listType=set
-	SourceSecurityGroups []string `json:"sourceSecurityGroups,omitempty"`
-	// The security groups that are allowed SSH access (port 22) to the worker nodes
-	EC2SSHKey string `json:"eC2SSHKey,omitempty"`
+	EC2SSHKey string `json:"ec2SSHKey,omitempty"`
 	// Use is a reference to an AWSCredentials object to use for authentication
 	// +kubebuilder:validation:Required
 	// +k8s:openapi-gen=false
