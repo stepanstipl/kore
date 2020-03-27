b98d12533e806437e3149a58e8cbb377eae89661
Andras Ferencz-Szabo <andras.ferencz-szabo@appvia.io>
2020-03-27 21:47:26 +0000

    Add all GKE cluster related parameters to the plan parameters

--- pkg/kore/assets/plan_schemas_test.go
+++ pkg/kore/assets/plan_schemas_test.go
@@ -18,26 +18,178 @@ package assets_test
 
 import (
 	"encoding/json"
-	"testing"
+
+	"github.com/appvia/kore/pkg/utils/validation"
 
 	"github.com/appvia/kore/pkg/kore/assets"
 	"github.com/appvia/kore/pkg/utils/jsonschema"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
 )
 
-func TestGKESchemaCompiles(t *testing.T) {
-	require.NotPanics(t, func() {
-		_ = jsonschema.Validate(assets.GKEPlanSchema, "test", nil)
+func sampleData() map[string]interface{} {
+	return map[string]interface{}{
+		"authorizedMasterNetworks": []map[string]interface{}{
+			{
+				"name": "default",
+				"cidr": "0.0.0.0/0",
+			},
+		},
+		"authProxyAllowedIPs":           []string{"0.0.0.0/0"},
+		"description":                   "This is a test cluster",
+		"diskSize":                      100,
+		"domain":                        "testdomain",
+		"enableAutoupgrade":             true,
+		"enableAutorepair":              true,
+		"enableAutoscaler":              true,
+		"enableDefaultTrafficBlock":     false,
+		"enableHTTPLoadBalancer":        true,
+		"enableHorizontalPodAutoscaler": true,
+		"enableIstio":                   false,
+		"enablePrivateEndpoint":         false,
+		"enablePrivateNetwork":          false,
+		"enableShieldedNodes":           true,
+		"enableStackDriverLogging":      true,
+		"enableStackDriverMetrics":      true,
+		"imageType":                     "COS",
+		"inheritTeamMembers":            false,
+		"machineType":                   "n1-standard-2",
+		"maintenanceWindow":             "03:00",
+		"maxSize":                       10,
+		"network":                       "default",
+		"region":                        "europe-west2",
+		"size":                          1,
+		"subnetwork":                    "default",
+		"version":                       "1.14.10-gke.24",
+	}
+}
+
+var nonEmptyFields = []string{
+	"description", "domain", "imageType", "machineType", "network", "region", "subnetwork", "version",
+}
+
+var _ = Describe("GKEPlanSchema", func() {
+
+	Context("The schema document", func() {
+		It("is a valid JSON document", func() {
+			var schema map[string]interface{}
+			err := json.Unmarshal([]byte(assets.GKEPlanSchema), &schema)
+			var context string
+			if err != nil {
+				if jsonErr, ok := err.(*json.SyntaxError); ok {
+					context = assets.GKEPlanSchema[jsonErr.Offset : jsonErr.Offset+100]
+				}
+			}
+			Expect(err).ToNot(HaveOccurred(), "error at: %s", context)
+		})
+
+		It("compiles", func() {
+			Expect(func() {
+				_ = jsonschema.Validate(assets.GKEPlanSchema, "test", nil)
+			}).ToNot(Panic())
+		})
+
+		It("is a valid JSON Schema Draft 07 document", func() {
+			var schema map[string]interface{}
+			err := json.Unmarshal([]byte(assets.GKEPlanSchema), &schema)
+			Expect(err).ToNot(HaveOccurred())
+
+			err = jsonschema.Validate(assets.JSONSchemaDraft07, "test", schema)
+			Expect(err).ToNot(HaveOccurred())
+		})
 	})
-}
 
-func TestGKESchemaIsValidDraft07Schema(t *testing.T) {
-	var schema map[string]interface{}
-	err := json.Unmarshal([]byte(assets.GKEPlanSchema), &schema)
-	require.NoError(t, err)
+	It("should validate the test data successfully by default", func() {
+		data := sampleData()
+		err := jsonschema.Validate(assets.GKEPlanSchema, "test", data)
+		Expect(err).ToNot(HaveOccurred())
+	})
 
-	err = jsonschema.Validate(assets.JSONSchemaDraft07, "test", schema)
-	assert.NoError(t, err, "GKEPlanSchema is not a valid")
-}
+	DescribeTable("Validating individual parameters",
+		func(field string, value interface{}, expectError bool) {
+			data := sampleData()
+			data[field] = value
+			err := jsonschema.Validate(assets.GKEPlanSchema, "test", data)
+			if expectError {
+				Expect(err).To(HaveOccurred())
+				Expect(err.(*validation.Error).FieldErrors).To(HaveLen(1))
+				Expect(err.(*validation.Error).FieldErrors[0].Field).To(HavePrefix(field))
+			} else {
+				Expect(err).ToNot(HaveOccurred())
+			}
+		},
+		Entry("description is empty", "description", "", true),
+		Entry("domain is empty", "description", "", true),
+		Entry("imageType is empty", "description", "", true),
+		Entry("machineType is empty", "description", "", true),
+		Entry("network is empty", "description", "", true),
+		Entry("region is empty", "description", "", true),
+		Entry("subnetwork is empty", "description", "", true),
+		Entry("version is empty", "description", "", true),
+		Entry("diskSize is less than minimum", "diskSize", 9, true),
+		Entry("diskSize is the minimum", "diskSize", 10, false),
+		Entry("diskSize is the maximum", "diskSize", 65536, false),
+		Entry("diskSize is more than maximum", "diskSize", 65537, true),
+		Entry("authorizedMasterNetworks with no elements", "authorizedMasterNetworks", []map[string]interface{}{}, true),
+		Entry("authorizedMasterNetworks with missing cidr", "authorizedMasterNetworks", []map[string]interface{}{
+			{"name": "xx"},
+		}, true),
+		Entry("authorizedMasterNetworks with missing name", "authorizedMasterNetworks", []map[string]interface{}{
+			{"cidr": "1.2.3.4/32"},
+		}, true),
+		Entry("authorizedMasterNetworks with invalid cidr", "authorizedMasterNetworks", []map[string]interface{}{
+			{"name": "xx", "cidr": "invalid"},
+		}, true),
+		Entry("authProxyAllowedIPs with no elements", "authProxyAllowedIPs", []string{}, true),
+		Entry("authProxyAllowedIPs with invalid value", "authProxyAllowedIPs", []string{"invalid"}, true),
+	)
+
+	Context("Validating multiple parameters", func() {
+		var data map[string]interface{}
+		var validationErr error
+
+		BeforeEach(func() {
+			data = sampleData()
+		})
+
+		JustBeforeEach(func() {
+			validationErr = jsonschema.Validate(assets.GKEPlanSchema, "test", data)
+		})
+
+		When("inheritTeamMembers is true", func() {
+			BeforeEach(func() {
+				data["inheritTeamMembers"] = true
+			})
+
+			When("defaultTeamRole is not set", func() {
+				It("throws a validation error", func() {
+					Expect(validationErr).To(HaveOccurred())
+					Expect(validationErr.(*validation.Error).FieldErrors).To(HaveLen(1))
+					Expect(validationErr.Error()).To(ContainSubstring("defaultTeamRole is required"))
+				})
+			})
+
+			When("defaultTeamRole is empty", func() {
+				BeforeEach(func() {
+					data["defaultTeamRole"] = ""
+				})
+				It("throws a validation error", func() {
+					Expect(validationErr).To(HaveOccurred())
+					Expect(validationErr.(*validation.Error).FieldErrors).To(HaveLen(1))
+					Expect(validationErr.Error()).To(ContainSubstring("defaultTeamRole: String length must be greater than or equal to 1"))
+				})
+			})
+
+			When("defaultTeamRole is not empty", func() {
+				BeforeEach(func() {
+					data["defaultTeamRole"] = "some role"
+				})
+				It("doesn't throw an error", func() {
+					Expect(validationErr).ToNot(HaveOccurred())
+				})
+			})
+		})
+	})
+
+})
