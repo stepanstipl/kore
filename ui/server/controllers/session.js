07ec6baaa812d56fa67d466c407d76b671c8961a
David Thompson <davethompson21@gmail.com>
2020-04-01 14:49:01 +0100

    Cluster access info fix
    * amending to new command `kubeconfig` instead of `auth`
    * showing CLI login command first

--- ui/server/controllers/session.js
+++ ui/server/controllers/session.js
@@ -8,9 +8,16 @@ function getSessionUser(orgService) {
   }
 }
 
-function initRouter({ ensureAuthenticated, ensureUserCurrent, persistRequestedPath, orgService }) {
+function getConfig(koreApi) {
+  return (req, res) => {
+    res.json({ apiUrl: koreApi.url })
+  }
+}
+
+function initRouter({ ensureAuthenticated, ensureUserCurrent, persistRequestedPath, orgService, koreApi }) {
   const router = Router()
   router.get('/session/user', ensureAuthenticated, ensureUserCurrent, persistRequestedPath, getSessionUser(orgService))
+  router.get('/session/config', ensureAuthenticated, ensureUserCurrent, getConfig(koreApi))
   return router
 }
 
