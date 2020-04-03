975f650da61c4cbcf15f8c43a8a8753d7a84d80c
Rohith <gambol99@gmail.com>
2020-04-03 17:26:07 +0100

    - fixing up the command line as the reorder doesn't process key=value correctly

--- cmd/kore-apiserver/main.go
+++ cmd/kore-apiserver/main.go
@@ -55,8 +55,7 @@ func main() {
 		},
 	}
 
-	koreCliApp := cmd.NewApp(app)
-	if err := koreCliApp.Run(os.Args); err != nil {
+	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintf(os.Stderr, "[error] %s\n", err)
 		os.Exit(1)
 	}
