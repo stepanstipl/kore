c99b6a6879149a8353ac326e001a3fbb57f8a956
Rohith <gambol99@gmail.com>
2020-04-11 19:46:49 +0100

    - cleaning up the code in the eks client

--- pkg/utils/cloud/aws/eks.go
+++ pkg/utils/cloud/aws/eks.go
@@ -17,10 +17,14 @@
 package aws
 
 import (
+	"context"
 	"errors"
+	"strings"
+	"time"
 
 	eksv1alpha1 "github.com/appvia/kore/pkg/apis/eks/v1alpha1"
 	"github.com/appvia/kore/pkg/kore"
+	"github.com/appvia/kore/pkg/utils"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -29,6 +33,13 @@ import (
 	"github.com/aws/aws-sdk-go/service/eks"
 	awseks "github.com/aws/aws-sdk-go/service/eks"
 	log "github.com/sirupsen/logrus"
+)
+
+var (
+	// ErrClusterNotFound indicates the cluster does not exist
+	ErrClusterNotFound = errors.New("eks cluster not found")
+	// ErrResourceBusy indicate the resource is currently busy performing an operation
+	ErrResourceBusy = errors.New("resource is busy performing an operation (upgrade, creating)")
 )
 
 // Client for aws EKS and EKS nodegroups
@@ -66,14 +77,16 @@ func NewBasicClient(cred *eksv1alpha1.EKSCredentials, clusterName, region string
 }
 
 // NewClient gets an AWS and cluster session with a reference to our API object
-// TODO: rename to show it's eks specific
-func NewClient(cred *eksv1alpha1.EKSCredentials, cluster *eksv1alpha1.EKS) (*Client, error) {
+func NewEKSClient(cred *eksv1alpha1.EKSCredentials, cluster *eksv1alpha1.EKS) (*Client, error) {
 	sesh, err := session.NewSession(&aws.Config{
-		Region:      aws.String(cluster.Spec.Region),
-		Credentials: credentials.NewStaticCredentials(cred.Spec.AccessKeyID, cred.Spec.SecretAccessKey, ""),
+		Region: aws.String(cluster.Spec.Region),
+		Credentials: credentials.NewStaticCredentials(
+			cred.Spec.AccessKeyID,
+			cred.Spec.SecretAccessKey,
+			"",
+		),
 	})
 	if err != nil {
-
 		return nil, err
 	}
 
@@ -88,7 +101,6 @@ func NewClient(cred *eksv1alpha1.EKSCredentials, cluster *eksv1alpha1.EKS) (*Cli
 
 // NewEKSClientFromVPC will create a new eks client from an VPCClient object
 func NewEKSClientFromVPC(c *VPCClient, clusterName string) *Client {
-
 	return &Client{
 		clusterName: clusterName,
 		Sess:        c.Sess,
@@ -97,18 +109,16 @@ func NewEKSClientFromVPC(c *VPCClient, clusterName string) *Client {
 }
 
 // Exists checks if a cluster exists
-func (c *Client) Exists() (exists bool, err error) {
-	_, err = c.svc.DescribeCluster(&awseks.DescribeClusterInput{
+func (c *Client) Exists(ctx context.Context) (exists bool, err error) {
+	_, err = c.svc.DescribeClusterWithContext(ctx, &awseks.DescribeClusterInput{
 		Name: aws.String(c.clusterName),
 	})
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
 			switch aerr.Code() {
 			case eks.ErrCodeResourceNotFoundException:
-
 				return false, nil
 			default:
-
 				return false, err
 			}
 		} else {
@@ -120,32 +130,145 @@ func (c *Client) Exists() (exists bool, err error) {
 }
 
 // Create creates an EKS cluster
-func (c *Client) Create() (*eks.CreateClusterOutput, error) {
-	output, err := c.svc.CreateCluster(c.createClusterInput())
+func (c *Client) Create(ctx context.Context) error {
+	logger := log.WithFields(log.Fields{
+		"name":      c.cluster.Name,
+		"namespace": c.cluster.Namespace,
+	})
+	logger.Debug("attempting to create the eks cluster")
+
+	// @step: we should check if the cluster already exist
+	existing, err := c.Exists(ctx)
 	if err != nil {
-		return nil, err
+		logger.WithError(err).Error("trying to check for the eks cluster")
+
+		return err
+	}
+	if !existing {
+		_, err := c.svc.CreateClusterWithContext(ctx, c.createClusterInput())
+		if err != nil {
+			return err
+		}
 	}
 
-	return output, nil
+	return c.WaitForClusterReady(ctx)
 }
 
-// Delete Delete an EKS cluster
-func (c *Client) Delete() (*eks.DeleteClusterOutput, error) {
-	input := &eks.DeleteClusterInput{
-		Name: &c.clusterName,
-	}
-	output, err := c.svc.DeleteCluster(input)
+// WaitForClusterReady waits for the eks cluster be go ready
+func (c *Client) WaitForClusterReady(ctx context.Context) error {
+	logger := log.WithFields(log.Fields{
+		"name":      c.cluster.Name,
+		"namespace": c.cluster.Namespace,
+	})
+	logger.Debug("attempting to wait for eks cluster to be ready")
+
+	// @step: check we are waiting for something that actaully exist
+	existing, err := c.Exists(ctx)
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			log.Debugf("aws specific error when trying to delete eks cluster from aws %s", aerr.Error())
-		} else {
-			log.WithError(err).Debug("error when trying to delete eks cluster from aws")
+		logger.WithError(err).Error("trying to check if cluster exists to wait for")
+
+		return err
+	}
+	if !existing {
+		logger.Warn("no eks cluster to wait for, something went wrong here")
+
+		return ErrClusterNotFound
+	}
+
+	// @step: wait for the cluster to be created
+	return utils.WaitUntilComplete(ctx, 1*time.Hour, 30*time.Second, func() (bool, error) {
+		resp, err := c.svc.DescribeClusterWithContext(ctx, &eks.DescribeClusterInput{
+			Name: aws.String(c.cluster.Name),
+		})
+		if err != nil {
+			logger.WithError(err).Error("trying to check for eks cluster status")
+
+			return false, nil
 		}
 
-		return nil, err
+		if resp.Cluster == nil {
+			logger.Warn("no cluster found in the describe response")
+
+			return false, nil
+		}
+
+		switch aws.StringValue(resp.Cluster.Status) {
+		case eks.ClusterStatusActive:
+			logger.Debug("eks cluster is active and ready")
+
+			return true, nil
+		case eks.ClusterStatusFailed:
+			return false, errors.New("cluster has failed to provision")
+		case eks.ClusterStatusCreating:
+			logger.Debug("cluster is still pending in provisioning")
+		default:
+			logger.Warnf("unknown cluster status: %s returned", aws.StringValue(resp.Cluster.Status))
+		}
+
+		return false, nil
+	})
+}
+
+// Delete is responsible for deleting the eks cluster
+func (c *Client) Delete(ctx context.Context) error {
+	logger := log.WithFields(log.Fields{
+		"name":      c.cluster.Name,
+		"namespace": c.cluster.Namespace,
+	})
+	logger.Debug("attempting to delete the eks cluster")
+
+	// @step: get the state of the cluster
+	resp, err := c.svc.DescribeClusterWithContext(ctx, &eks.DescribeClusterInput{
+		Name: aws.String(c.cluster.Name),
+	})
+	if err != nil {
+		if !c.IsNotFound(err) {
+			logger.WithError(err).Error("truing to describe the eks cluster")
+
+			return err
+		}
+
+		return nil
 	}
 
-	return output, nil
+	// @step: if the cluster is not deleting, try and delete now
+	switch aws.StringValue(resp.Cluster.Status) {
+	case eks.ClusterStatusActive, eks.ClusterStatusFailed:
+		if _, err := c.svc.DeleteClusterWithContext(ctx, &eks.DeleteClusterInput{
+			Name: aws.String(c.clusterName),
+		}); err != nil {
+			log.WithError(err).Error("trying to delete eks cluster from aws")
+
+			return err
+		}
+	case eks.ClusterStatusCreating:
+		logger.Debug("eks cluster is still being created, cannot be deleted yet")
+
+		return errors.New("eks is still being created, cannot delete yet")
+	case eks.ClusterStatusUpdating:
+		logger.Debug("eks cluster is still being created, cannot be deleted yet")
+
+		return errors.New("eks is still being updated, cannot delete yet")
+	}
+
+	// @step: we need to wait for the cluster to be remove
+	utils.WaitUntilComplete(context.Background(), 1*time.Hour, 30*time.Second, func() (bool, error) {
+		logger.Debug("checking if the eks cluster has been deleted")
+
+		if found, err := c.Exists(ctx); err != nil {
+			logger.WithError(err).Error("trying to check for eks cluster")
+
+			return false, nil
+		} else if !found {
+			logger.Debug("eks cluster has been successfully removed")
+
+			return true, nil
+		}
+
+		return false, nil
+	})
+
+	return nil
 }
 
 // Update should migrate changes to a cluster object
@@ -160,38 +283,12 @@ func (c *Client) VerifyCredentials() error {
 	return nil
 }
 
-// createClusterInput is used to generate the EKS cluster definition
-func (c *Client) createClusterInput() *awseks.CreateClusterInput {
-	d := &awseks.CreateClusterInput{
-		Name:    aws.String(c.cluster.Name),
-		RoleArn: aws.String(c.cluster.Status.RoleARN),
-		Version: aws.String(c.cluster.Spec.Version),
-		ResourcesVpcConfig: &awseks.VpcConfigRequest{
-			SecurityGroupIds:      aws.StringSlice(c.cluster.Spec.SecurityGroupIDs),
-			SubnetIds:             aws.StringSlice(c.cluster.Spec.SubnetIDs),
-			EndpointPublicAccess:  aws.Bool(true),
-			EndpointPrivateAccess: aws.Bool(true),
-		},
-		Tags: map[string]*string{
-			kore.Label("name"):  aws.String(c.cluster.Name),
-			kore.Label("owned"): aws.String("true"),
-		},
-	}
-
-	for _, x := range c.cluster.Spec.AuthorizedMasterNetworks {
-		d.ResourcesVpcConfig.PublicAccessCidrs = append(d.ResourcesVpcConfig.PublicAccessCidrs, aws.String(x))
-	}
-
-	return d
-}
-
-// DescribeEKS returns the AWS EKS output
-func (c *Client) DescribeEKS() (*eks.Cluster, error) {
-	d, err := c.svc.DescribeCluster(&awseks.DescribeClusterInput{
+// Describe returns the AWS EKS output
+func (c *Client) Describe(ctx context.Context) (*eks.Cluster, error) {
+	d, err := c.svc.DescribeClusterWithContext(ctx, &awseks.DescribeClusterInput{
 		Name: aws.String(c.clusterName),
 	})
 	if err != nil {
-
 		return nil, err
 	}
 
@@ -199,17 +296,76 @@ func (c *Client) DescribeEKS() (*eks.Cluster, error) {
 }
 
 // DeleteNodeGroup will remove a nodegroup from a cluster
-func (c *Client) DeleteNodeGroup(nodegroup *eksv1alpha1.EKSNodeGroup) error {
-	_, err := c.svc.DeleteNodegroup(&eks.DeleteNodegroupInput{
-		ClusterName:   &c.clusterName,
-		NodegroupName: &nodegroup.Name,
+func (c *Client) DeleteNodeGroup(ctx context.Context, group *eksv1alpha1.EKSNodeGroup) error {
+	logger := log.WithFields(log.Fields{
+		"name":      group.Name,
+		"namespace": group.Namespace,
+	})
+	logger.Debug("attempting to delete the eks nodegroup")
+
+	// @step: check the status of the nodegroup
+	resp, err := c.svc.DescribeNodegroupWithContext(ctx, &eks.DescribeNodegroupInput{
+		ClusterName:   aws.String(group.Spec.Cluster.Name),
+		NodegroupName: aws.String(group.Name),
+	})
+	if err != nil {
+		if !c.IsNotFound(err) {
+			logger.WithError(err).Error("trying to describe the nodegroup")
+
+			return err
+		}
+
+		return nil
+	}
+
+	// @step: we check the status if see if its already deleting
+	switch aws.StringValue(resp.Nodegroup.Status) {
+	case eks.NodegroupStatusActive, eks.NodegroupStatusCreateFailed, eks.NodegroupStatusDegraded:
+		_, err := c.svc.DeleteNodegroupWithContext(ctx, &eks.DeleteNodegroupInput{
+			ClusterName:   aws.String(group.Spec.Cluster.Name),
+			NodegroupName: aws.String(group.Name),
+		})
+
+		if err != nil {
+			if c.IsNotFound(err) {
+				return nil
+			}
+			logger.WithError(err).Error("trying to delete the nodegroup")
+
+			return err
+		}
+	case eks.NodegroupStatusCreating, eks.NodegroupStatusUpdating:
+		log.Warn("trying to delete nodegroup, resource operating pending")
+
+		return ErrResourceBusy
+	case eks.NodegroupStatusDeleteFailed:
+		log.Error("trying to delete the nodegroup")
+
+		return errors.New("nodegroup has failed to delete, please check console")
+	}
+
+	// @step: we need to wait for the nodegroup to be removed
+	utils.WaitUntilComplete(context.Background(), 30*time.Minute, 30*time.Second, func() (bool, error) {
+		logger.Debug("checking if the eks nodegroup has been deleted")
+
+		if found, err := c.NodeGroupExists(ctx, group); err != nil {
+			logger.WithError(err).Error("trying to check for eks nodegroup")
+
+			return false, nil
+		} else if !found {
+			logger.Debug("eks nodegroup has been successfully removed")
+
+			return true, nil
+		}
+
+		return false, nil
 	})
 
 	return err
 }
 
 // CreateNodeGroup will create a node group for the EKS cluster
-func (c *Client) CreateNodeGroup(nodegroup *eksv1alpha1.EKSNodeGroup) error {
+func (c *Client) CreateNodeGroup(ctx context.Context, nodegroup *eksv1alpha1.EKSNodeGroup) error {
 	input := &eks.CreateNodegroupInput{
 		AmiType:        aws.String(nodegroup.Spec.AMIType),
 		ClusterName:    aws.String(nodegroup.Spec.Cluster.Name),
@@ -237,41 +393,26 @@ func (c *Client) CreateNodeGroup(nodegroup *eksv1alpha1.EKSNodeGroup) error {
 	if len(nodegroup.Spec.Labels) > 0 {
 		input.Labels = aws.StringMap(nodegroup.Spec.Labels)
 	}
-	_, err := c.svc.CreateNodegroup(input)
-	if err != nil {
-		// TODO - oh my, more from appvia/eks-operator
-		if aerr, ok := err.(awserr.Error); ok {
-			log.Debugf("aws specific error when trying to delete eks cluster from aws %s", aerr.Error())
-		} else {
-			log.WithError(err).Debug("error when trying to delete eks cluster from aws")
-		}
 
+	if _, err := c.svc.CreateNodegroup(input); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-// NodeGroupExists TODO - looks wrong should probably list
-func (c *Client) NodeGroupExists(nodegroup *eksv1alpha1.EKSNodeGroup) (exists bool, err error) {
-	_, err = c.svc.DescribeNodegroup(&eks.DescribeNodegroupInput{
-		ClusterName:   &c.clusterName,
-		NodegroupName: &nodegroup.Name,
+// NodeGroupExists is responsible for checking if the nodegroup exists
+func (c *Client) NodeGroupExists(ctx context.Context, nodegroup *eksv1alpha1.EKSNodeGroup) (exists bool, err error) {
+	_, err = c.svc.DescribeNodegroupWithContext(ctx, &eks.DescribeNodegroupInput{
+		ClusterName:   aws.String(nodegroup.Spec.Cluster.Name),
+		NodegroupName: aws.String(nodegroup.Name),
 	})
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case eks.ErrCodeResourceNotFoundException:
-
-				return false, nil
-			default:
-
-				return false, err
-			}
-		} else {
-
+		if !c.IsNotFound(err) {
 			return false, err
 		}
+
+		return false, nil
 	}
 
 	return true, nil
@@ -302,4 +443,50 @@ func (c *Client) GetEKSNodeGroupStatus(nodegroup *eksv1alpha1.EKSNodeGroup) (sta
 	})
 
 	return *out.Nodegroup.Status, err
+}
+
+// createClusterInput is used to generate the EKS cluster definition
+func (c *Client) createClusterInput() *awseks.CreateClusterInput {
+	d := &awseks.CreateClusterInput{
+		Name:    aws.String(c.cluster.Name),
+		RoleArn: aws.String(c.cluster.Status.RoleARN),
+		Version: aws.String(c.cluster.Spec.Version),
+		ResourcesVpcConfig: &awseks.VpcConfigRequest{
+			SecurityGroupIds:      aws.StringSlice(c.cluster.Spec.SecurityGroupIDs),
+			SubnetIds:             aws.StringSlice(c.cluster.Spec.SubnetIDs),
+			EndpointPublicAccess:  aws.Bool(true),
+			EndpointPrivateAccess: aws.Bool(true),
+		},
+		Tags: map[string]*string{
+			kore.Label("name"):  aws.String(c.cluster.Name),
+			kore.Label("owned"): aws.String("true"),
+			kore.Label("team"):  aws.String(c.cluster.Namespace),
+		},
+	}
+
+	for _, x := range c.cluster.Spec.AuthorizedMasterNetworks {
+		d.ResourcesVpcConfig.PublicAccessCidrs = append(d.ResourcesVpcConfig.PublicAccessCidrs, aws.String(x))
+	}
+
+	return d
+}
+
+func (c *Client) IsNotFound(err error) bool {
+	if aerr, ok := err.(awserr.Error); ok {
+		if aerr.Code() == eks.ErrCodeResourceNotFoundException {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (c *Client) IsInvalidParameterException(err error) bool {
+	if aerr, ok := err.(awserr.Error); ok {
+		if aerr.Code() == eks.ErrCodeInvalidParameterException && strings.Contains(aerr.Message(), "does not exist") {
+			return true
+		}
+	}
+
+	return false
 }
