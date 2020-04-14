7b76f3b23b633e4f1cb53bc94c228ede510e8c0a
Rohith <gambol99@gmail.com>
2020-04-14 11:23:19 +0100

    - adding the jsonpath wrapper to include the sjoin

---
+++ pkg/utils/jsonpath/jsonpath.go
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2020 Appvia Ltd <info@appvia.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jsonpath
+
+import (
+	"github.com/tidwall/gjson"
+)
+
+// IsValid checks the json is valid
+func IsValid(data string) bool {
+	return gjson.Valid(data)
+}
+
+// Get returns the value
+func Get(data string, key string) gjson.Result {
+	return gjson.Get(data, key)
+}
+
+// GetMany returns the value
+func GetMany(data string, paths ...string) []gjson.Result {
+	return gjson.GetMany(data, paths...)
+}
+
+// Parse is used to parse the json
+func Parse(data string) gjson.Result {
+	return gjson.Parse(data)
+}
