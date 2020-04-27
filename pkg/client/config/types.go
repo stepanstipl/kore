aff107794c44c475bf5547f3731ed942e17dd5ce
Rohith <gambol99@gmail.com>
2020-04-27 23:38:32 +0100

    OmitEmpty YAML tags
    Adding the omitempty to remove the unrequired fields

--- pkg/client/config/types.go
+++ pkg/client/config/types.go
@@ -26,75 +26,75 @@ const (
 // Config is the configuration for the api
 type Config struct {
 	// AuthInfos is a collection of credentials
-	AuthInfos map[string]*AuthInfo `json:"users,omitempty" yaml:"users"`
+	AuthInfos map[string]*AuthInfo `json:"users,omitempty" yaml:"users,omitempty"`
 	// CurrentProfile is the profile in use at the moment
-	CurrentProfile string `json:"current-profile,omitempty" yaml:"current-profile"`
+	CurrentProfile string `json:"current-profile,omitempty" yaml:"current-profile,omitempty"`
 	// Profiles is a collection of profiles
-	Profiles map[string]*Profile `json:"profiles,omitempty" yaml:"profiles"`
+	Profiles map[string]*Profile `json:"profiles,omitempty" yaml:"profiles,omitempty"`
 	// Servers is a collection of api endpoints
-	Servers map[string]*Server `json:"servers,omitempty" yaml:"servers"`
+	Servers map[string]*Server `json:"servers,omitempty" yaml:"servers,omitempty"`
 	// Version is the version of the configuration
-	Version string `json:"version,omitempty" yaml:"version"`
+	Version string `json:"version,omitempty" yaml:"version,omitempty"`
 	// FeatureGates shows all feature gates
-	FeatureGates map[string]bool `json:"feature-gates,omitempty" yaml:"feature-gates"`
+	FeatureGates map[string]bool `json:"feature-gates,omitempty" yaml:"feature-gates,omitempty"`
 }
 
 // AuthInfo defines a credential to the api endpoint
 type AuthInfo struct {
 	// BasicAuth defines a basic auth user/pass credential
-	BasicAuth *BasicAuth `json:"basic-auth,omitempty" yaml:"basic-auth"`
+	BasicAuth *BasicAuth `json:"basic-auth,omitempty" yaml:"basic-auth,omitempty"`
 	// Token is a static token to use
-	Token *string `json:"token,omitempty" yaml:"token"`
+	Token *string `json:"token,omitempty" yaml:"token,omitempty"`
 	// OIDC is credentials from an oauth2 provider
-	OIDC *OIDC `json:"oidc,omitempty" yaml:"oidc"`
+	OIDC *OIDC `json:"oidc,omitempty" yaml:"oidc,omitempty"`
 }
 
 // BasicAuth defines a basic user credential
 type BasicAuth struct {
 	// Username is the username for authentication
-	Username string `json:"username,omitempty" yaml:"username"`
+	Username string `json:"username,omitempty" yaml:"username,omitempty"`
 	// Password is the user password
-	Password string `json:"password,omitempty" yaml:"password"`
+	Password string `json:"password,omitempty" yaml:"password,omitempty"`
 }
 
 // OIDC is the identity within the kore
 type OIDC struct {
 	// AccessToken is the access token retrieved from kore
-	AccessToken string `json:"access-token,omitempty" yaml:"access-token"`
+	AccessToken string `json:"access-token,omitempty" yaml:"access-token,omitempty"`
 	// AuthorizeURL is the endpoint for the authorize
-	AuthorizeURL string `json:"authorize-url,omitempty" yaml:"authorize-url"`
+	AuthorizeURL string `json:"authorize-url,omitempty" yaml:"authorize-url,omitempty"`
 	// ClientID is the client id for the user
-	ClientID string `json:"client-id,omitempty" yaml:"client-id"`
+	ClientID string `json:"client-id,omitempty" yaml:"client-id,omitempty"`
 	// ClientSecret is the client secret used for authentication
-	ClientSecret string `json:"client-secret,omitempty" yaml:"client-secret"`
+	ClientSecret string `json:"client-secret,omitempty" yaml:"client-secret,omitempty"`
 	// IDToken the identity token
-	IDToken string `json:"id-token,omitempty" yaml:"id-token"`
+	IDToken string `json:"id-token,omitempty" yaml:"id-token,omitempty"`
 	// RefreshToken is refresh token for the user
-	RefreshToken string `json:"refresh-token,omitempty" yaml:"refresh-token"`
+	RefreshToken string `json:"refresh-token,omitempty" yaml:"refresh-token,omitempty"`
 	// TokenURL is the endpoint for tokens
-	TokenURL string `json:"token-url,omitempty" yaml:"token-url"`
+	TokenURL string `json:"token-url,omitempty" yaml:"token-url,omitempty"`
 }
 
 // Profile links endpoint and a credential together
 type Profile struct {
 	// AuthInfo is the credentials to use
-	AuthInfo string `json:"user,omitempty" yaml:"user"`
+	AuthInfo string `json:"user,omitempty" yaml:"user,omitempty"`
 	// Server is a reference to the server config
-	Server string `json:"server,omitempty" yaml:"server"`
+	Server string `json:"server,omitempty" yaml:"server,omitempty"`
 	// Team is the default team for this profile
-	Team string `json:"team,omitempty" yaml:"team"`
+	Team string `json:"team,omitempty" yaml:"team,omitempty"`
 }
 
 // Server defines an endpoint for the api server
 type Server struct {
 	// Endpoint the url for the api endpoint of kore
-	Endpoint string `json:"server,omitempty" yaml:"server"`
+	Endpoint string `json:"server,omitempty" yaml:"server,omitempty"`
 }
 
 // RefreshResponse is the response returned when a token is refreshed
 type RefreshResponse struct {
 	// AccessToken is the access token provided
-	AccessToken string `json:"access_token,omitempty" yaml:"access_token"`
+	AccessToken string `json:"access_token,omitempty" yaml:"access_token,omitempty"`
 	// IDToken string is the identity token
-	IDToken string `json:"id_token,omitempty" yaml:"id_token"`
+	IDToken string `json:"id_token,omitempty" yaml:"id_token,omitempty"`
 }
