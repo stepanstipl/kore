1ace0124cddb6c32ae2cec89eb45afa563c57caf
Rohith <gambol99@gmail.com>
2020-05-13 12:51:58 +0100

    GKE Auto Repair Security Check
    - adding another simple check to indicate the status of the auto repair

---
+++ pkg/security/gke_autorepair.go
@@ -0,0 +1,107 @@
+/**
+ * Copyright 2020 Appvia Ltd <info@appvia.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package security
+
+import (
+	"context"
+	"errors"
+
+	clustersv1 "github.com/appvia/kore/pkg/apis/clusters/v1"
+	configv1 "github.com/appvia/kore/pkg/apis/config/v1"
+	securityv1 "github.com/appvia/kore/pkg/apis/security/v1"
+
+	"github.com/tidwall/gjson"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// GKEAutorepair determines whether the auth proxy IP range is suitably limited
+type GKEAutorepair struct{}
+
+// Implement Rule
+
+// Code returns the idenfier for this rule
+func (p *GKEAutorepair) Code() string {
+	return "GKE-03"
+}
+
+// Name returns the name of this rule
+func (p *GKEAutorepair) Name() string {
+	return "GKE Autorepair"
+}
+
+// Description returns the markdown-formatted description of this rule
+func (p *GKEAutorepair) Description() string {
+	return `
+## Overview
+
+This rule checks the status of the auto repair on the GKE plans or clusters.
+
+## Details
+
+Autorepair on GKE permits the control plan to automatically replace nodes which have failed the
+kubernetes health checks.
+
+## Impact of warnings from this rule
+
+Not having this enabled means you may have unschedulable nodes or nodes with health concerned.
+`
+}
+
+// ensureFeature handles the feature for both plans anc clusters
+func (p *GKEAutorepair) ensureFeature(config string) (*securityv1.SecurityScanRuleResult, error) {
+	result := &securityv1.SecurityScanRuleResult{
+		RuleCode: p.Code(),
+		Status:   securityv1.Warning,
+	}
+	feature := gjson.Get(string(config), "enableAutorepair")
+	if !feature.Exists() {
+		result.Message = "Could not check cluster due to invalid JSON"
+
+		return result, errors.New("enableAutorepair parameter does not exist")
+	}
+
+	// @step: check if the cluster version is out-of-date
+	if !feature.Bool() {
+		result.Message = "GKE Autorepair is disabled "
+		result.Status = securityv1.Warning
+
+		return result, nil
+	}
+
+	result.Message = "GKE Autorepair is enabled"
+	result.Status = securityv1.Compliant
+
+	return result, nil
+}
+
+// CheckPlan checks a plan for compliance with this rule
+func (p *GKEAutorepair) CheckPlan(ctx context.Context, cc client.Client, target *configv1.Plan) (*securityv1.SecurityScanRuleResult, error) {
+	if target.Spec.Kind != "GKE" {
+		return nil, nil
+	}
+
+	return p.ensureFeature(string(target.Spec.Configuration.Raw))
+}
+
+// CheckCluster checks a cluster for compliance with this rule
+func (p *GKEAutorepair) CheckCluster(ctx context.Context, cc client.Client, target *clustersv1.Cluster) (*securityv1.SecurityScanRuleResult, error) {
+	if target.Spec.Kind != "GKE" {
+		return nil, nil
+	}
+
+	return p.ensureFeature(string(target.Spec.Configuration.Raw))
+}
---
+++ pkg/security/gke_autorepair_test.go
@@ -0,0 +1,108 @@
+/**
+ * Copyright 2020 Appvia Ltd <info@appvia.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package security_test
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+
+	configv1 "github.com/appvia/kore/pkg/apis/config/v1"
+	securityv1 "github.com/appvia/kore/pkg/apis/security/v1"
+	"github.com/appvia/kore/pkg/security"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	log "github.com/sirupsen/logrus"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func init() {
+	log.SetOutput(ioutil.Discard)
+}
+
+var _ = Describe("GKE Autoscaling Rule", func() {
+	scanner := security.NewEmpty()
+
+	BeforeEach(func() {
+		scanner.RegisterRule(&security.GKEAutorepair{})
+	})
+
+	renderPlan := func(values map[string]interface{}) *configv1.Plan {
+		c, _ := json.Marshal(values)
+
+		return &configv1.Plan{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: configv1.GroupVersion.String(),
+				Kind:       "Plan",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "test",
+				Name:      "test1",
+			},
+			Spec: configv1.PlanSpec{
+				Configuration: apiextv1.JSON{Raw: c},
+			},
+		}
+	}
+
+	Describe("CheckPlan", func() {
+		When("called with a plan which not related to gke", func() {
+			It("should return an ignore", func() {
+				p := renderPlan(map[string]interface{}{})
+				p.Spec.Kind = "EKS"
+				result := scanner.ScanPlan(context.Background(), fake.NewFakeClient(), p)
+
+				Expect(result.Spec.OverallStatus).To(Equal(securityv1.Compliant))
+				Expect(len(result.Spec.Results)).To(Equal(0))
+			})
+		})
+
+		Context("GKE Plan", func() {
+			plan := renderPlan(map[string]interface{}{})
+			plan.Spec.Kind = "GKE"
+
+			When("called with GKE kind but no enableAutorepair value", func() {
+				It("should not have a result", func() {
+					result := scanner.ScanPlan(context.Background(), fake.NewFakeClient(), plan)
+					Expect(result.Spec.OverallStatus).To(Equal(securityv1.Compliant))
+					Expect(len(result.Spec.Results)).To(Equal(0))
+				})
+			})
+
+			When("called with GKE kind and enableAutorepair is false", func() {
+				It("should be non-compliant", func() {
+					plan.Spec.Configuration.Raw = []byte(`{"enableAutorepair":false}`)
+
+					result := scanner.ScanPlan(context.Background(), fake.NewFakeClient(), plan)
+					Expect(result.Spec.OverallStatus).To(Equal(securityv1.Warning))
+				})
+			})
+
+			When("called with GKE kind and enableAutorepair is true", func() {
+				It("should be compliant", func() {
+					plan.Spec.Configuration.Raw = []byte(`{"enableAutorepair":true}`)
+
+					result := scanner.ScanPlan(context.Background(), fake.NewFakeClient(), plan)
+					Expect(result.Spec.OverallStatus).To(Equal(securityv1.Compliant))
+				})
+			})
+		})
+	})
+})
