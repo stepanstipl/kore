1e02b79ac07f18bb623ba2e2b3166208b23b37fc
Rohith <gambol99@gmail.com>
2020-05-21 15:52:09 +0100

    Security Scan Deletion
    - fixing up the deletion of the scans by archiving the deleted

--- pkg/controllers/security/reconcile.go
+++ pkg/controllers/security/reconcile.go
@@ -21,6 +21,7 @@ import (
 
 	clustersv1 "github.com/appvia/kore/pkg/apis/clusters/v1"
 	configv1 "github.com/appvia/kore/pkg/apis/config/v1"
+	"github.com/appvia/kore/pkg/utils/kubernetes"
 
 	log "github.com/sirupsen/logrus"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
@@ -33,8 +34,8 @@ func (c *Controller) Reconcile(request reconcile.Request) (reconcile.Result, err
 
 	logger := c.logger.WithFields(log.Fields{
 		"kind":      c.kind,
-		"name":      request.NamespacedName.Name,
-		"namespace": request.NamespacedName.Namespace,
+		"name":      request.Name,
+		"namespace": request.Namespace,
 	})
 	logger.Debug("attempting to reconcile the security scans")
 
@@ -49,7 +50,14 @@ func (c *Controller) Reconcile(request reconcile.Request) (reconcile.Result, err
 		return reconcile.Result{}, err
 	}
 
-	var err error
+	meta, err := kubernetes.GetMeta(t)
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+	if !meta.GetDeletionTimestamp().IsZero() {
+		return c.Delete(t)
+	}
+
 	switch c.kind {
 	case "Cluster":
 		err = c.kore.Security().ScanCluster(ctx, c.mgr.GetClient(), t.(*clustersv1.Cluster))
