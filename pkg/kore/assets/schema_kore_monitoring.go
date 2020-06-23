061f6e3abaf69a0b250fad2b4ec3609b502c42fe
Rohith <gambol99@gmail.com>
2020-06-23 11:59:26 +0100

    - placing the schema as an annotation for now

--- pkg/kore/assets/schema_kore_monitoring.go
+++ pkg/kore/assets/schema_kore_monitoring.go
@@ -33,101 +33,101 @@ kind: ServicePlan
 metadata:
   name: helm-app-kore-monitoring
   namespace: kore
-	annotations:
-		helm.values.schema: |
-			{
-				"$id": "https://appvia.io/schemas/kore-monitoring/plan.json",
-				"$schema": "http://json-schema.org/draft-07/schema#",
-				"description": "Kore Monitoring",
-				"type": "object",
-				"additionalProperties": false,
-				"required": [
-					"notifications"
-				],
-				"properties": {
-					"notifications": {
-						"type": "object",
-						"description": "Notification configuration is where to send the alerts",
-						"additionalProperties": false,
-						"properties": {
-							"slack": {
-								"type": "array",
-								"descriptions": "Configuration for Slack notifications",
-								"items": {
-									"type": "object",
-									"additionalProperties": false,
-									"required": [
-										"api_url"
-									],
-									"properties": {
-										"api_url": {
-											"type": "string",
-											"description": "The slack wehbook url used to send notifications",
-											"minLength": 1
-										},
-										"channel": {
-											"type": "string",
-											"description": "An optional channel to send the message to",
-											"minLength": 1
-										},
-										"title": {
-											"type": "string",
-											"description": "The title to be used on the alerts"
-										},
-										"description": {
-											"type": "string",
-											"description": "An optional description for this notification channel"
-										},
-										"priority": {
-											"type": "array",
-											"description": "An optional priority for this notificaton",
-											"items": {
-												"type": "string",
-												"minLength": 1,
-												"enum": [
-													"P1",
-													"P2",
-													"P3",
-													"P4"
-												]
-											},
-											"minItems": 1
-										},
-										"tags": {
-											"type": "array",
-											"description": "A collection of tags added to the notifications i.e. P1, P2",
-											"items": {
-												"type": "string",
-												"minLength": 1
-											}
-										}
-									}
-								}
-							},
-							"pagerduty": {
-								"type": "array",
-								"description": "Configuration for notifications to Pagerduty",
-								"items": {
-									"type": "object",
-									"additionalProperties": false,
-									"properties": {
-										"routing_key": {
-											"type": "string",
-											"description": "The PagerDuty integration key (when using PagerDuty integration type Events API v2)",
-											"minLength": 1
-										},
-										"service_key": {
-											"type": "string",
-											"description": "The PagerDuty integration key (when using PagerDuty integration type Prometheus)",
-											"minLength": 1
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
+  annotations:
+    helm.values.schema: |
+      {
+        "$id": "https://appvia.io/schemas/kore-monitoring/plan.json",
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "description": "Kore Monitoring",
+        "type": "object",
+        "additionalProperties": false,
+        "required": [
+          "notifications"
+        ],
+        "properties": {
+          "notifications": {
+            "type": "object",
+            "description": "Notification configuration is where to send the alerts",
+            "additionalProperties": false,
+            "properties": {
+              "slack": {
+                "type": "array",
+                "descriptions": "Configuration for Slack notifications",
+                "items": {
+                  "type": "object",
+                  "additionalProperties": false,
+                  "required": [
+                    "api_url"
+                  ],
+                  "properties": {
+                    "api_url": {
+                      "type": "string",
+                      "description": "The slack wehbook url used to send notifications",
+                      "minLength": 1
+                    },
+                    "channel": {
+                      "type": "string",
+                      "description": "An optional channel to send the message to",
+                      "minLength": 1
+                    },
+                    "title": {
+                      "type": "string",
+                      "description": "The title to be used on the alerts"
+                    },
+                    "description": {
+                      "type": "string",
+                      "description": "An optional description for this notification channel"
+                    },
+                    "priority": {
+                      "type": "array",
+                      "description": "An optional priority for this notificaton",
+                      "items": {
+                        "type": "string",
+                        "minLength": 1,
+                        "enum": [
+                          "P1",
+                          "P2",
+                          "P3",
+                          "P4"
+                        ]
+                      },
+                      "minItems": 1
+                    },
+                    "tags": {
+                      "type": "array",
+                      "description": "A collection of tags added to the notifications i.e. P1, P2",
+                      "items": {
+                        "type": "string",
+                        "minLength": 1
+                      }
+                    }
+                  }
+                }
+              },
+              "pagerduty": {
+                "type": "array",
+                "description": "Configuration for notifications to Pagerduty",
+                "items": {
+                  "type": "object",
+                  "additionalProperties": false,
+                  "properties": {
+                    "routing_key": {
+                      "type": "string",
+                      "description": "The PagerDuty integration key (when using PagerDuty integration type Events API v2)",
+                      "minLength": 1
+                    },
+                    "service_key": {
+                      "type": "string",
+                      "description": "The PagerDuty integration key (when using PagerDuty integration type Prometheus)",
+                      "minLength": 1
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
 spec:
   kind: helm-app
   summary: Managaed Cluster Monitoring
