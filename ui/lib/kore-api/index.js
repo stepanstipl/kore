2ea4ee0dd10f7d0a930f43a7a295aed0bc745aec
David Thompson <davethompson21@gmail.com>
2020-07-14 10:42:20 +0100

    Throw error if ctx is missing from server-side KoreApi client

--- ui/lib/kore-api/index.js
+++ ui/lib/kore-api/index.js
@@ -27,6 +27,9 @@ class KoreApi {
           // If we're running on the server, we need to layer in the user's identity to 
           // the request. When running in the browser, this is handled by the cookie-based
           // session.
+          if (!process.browser && !ctx) {
+            throw new Error('KoreApi client requires ctx containing id_token OR passport session data')
+          }
           if (!process.browser && ctx) {
             if (ctx.id_token || ctx.req) {
               req.headers['Authorization'] = `Bearer ${ctx.id_token || ctx.req.session.passport.user.id_token}`
