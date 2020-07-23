0912180fab9e2eb3487c92a711d79182f035a9ac
Lewis Marshall <lewis.marshall@appvia.io>
2020-07-23 11:31:19 +0100

    added api for retrieveing aws organizational units for accounts

--- pkg/kore/aws_organizations.go
+++ pkg/kore/aws_organizations.go
@@ -18,23 +18,40 @@ package kore
 
 import (
 	"context"
+	"fmt"
 
-	aws "github.com/appvia/kore/pkg/apis/aws/v1alpha1"
+	awsv1alpha1 "github.com/appvia/kore/pkg/apis/aws/v1alpha1"
 	"github.com/appvia/kore/pkg/store"
+	"github.com/aws/aws-sdk-go/aws"
 
+	awsutils "github.com/appvia/kore/pkg/utils/cloud/aws"
 	log "github.com/sirupsen/logrus"
 )
+
+// OUParams are the values required when querying OU's
+type OUParams struct {
+	// AWSKey is the AWS key used for carrying out the OU query
+	AWSKey string
+	// AWSKeyID is the AWS key ID used for carrying out the query
+	AWSKeyID string
+	// Region is the region to use for all Control Tower and organization APIS.
+	Region string
+	// RoleARN is the role to assume in the Master account
+	RoleARN string
+}
 
 // AWSOrganizations are the aws account orgs
 type AWSOrganizations interface {
 	// Delete is responsible for deleting a aws organisation
-	Delete(context.Context, string) (*aws.AWSOrganization, error)
+	Delete(context.Context, string) (*awsv1alpha1.AWSOrganization, error)
 	// Get return the definition from the api
-	Get(context.Context, string) (*aws.AWSOrganization, error)
+	Get(context.Context, string) (*awsv1alpha1.AWSOrganization, error)
 	// List returns all the aws orgaisations in the team
-	List(context.Context) (*aws.AWSOrganizationList, error)
+	List(context.Context) (*awsv1alpha1.AWSOrganizationList, error)
 	// Update is used to update the aws orgaisation definition
-	Update(context.Context, *aws.AWSOrganization) (*aws.AWSOrganization, error)
+	Update(context.Context, *awsv1alpha1.AWSOrganization) (*awsv1alpha1.AWSOrganization, error)
+	//GetAccGetAccountOUs will get OU's for Accounts given an awskey, awskeyid, region and roleArn
+	GetAccountOUs(context.Context, OUParams) ([]string, error)
 }
 
 type awsocl struct {
@@ -44,7 +61,7 @@ type awsocl struct {
 }
 
 // Update is responsible for update an aws org in kore
-func (h awsocl) Update(ctx context.Context, org *aws.AWSOrganization) (*aws.AWSOrganization, error) {
+func (h awsocl) Update(ctx context.Context, org *awsv1alpha1.AWSOrganization) (*awsv1alpha1.AWSOrganization, error) {
 	org.Namespace = h.team
 
 	err := h.Store().Client().Update(ctx,
@@ -62,9 +79,9 @@ func (h awsocl) Update(ctx context.Context, org *aws.AWSOrganization) (*aws.AWSO
 }
 
 // Delete is used to delete a project org from kore
-func (h awsocl) Delete(ctx context.Context, name string) (*aws.AWSOrganization, error) {
+func (h awsocl) Delete(ctx context.Context, name string) (*awsv1alpha1.AWSOrganization, error) {
 	// @step: does the orgaisation even exist
-	org := &aws.AWSOrganization{}
+	org := &awsv1alpha1.AWSOrganization{}
 	if err := h.Store().Client().Get(ctx,
 		store.GetOptions.InNamespace(h.team),
 		store.GetOptions.InTo(org),
@@ -76,7 +93,7 @@ func (h awsocl) Delete(ctx context.Context, name string) (*aws.AWSOrganization, 
 	}
 
 	// @step: are the any project claims referring to this org
-	claims := &aws.AWSAccountClaimList{}
+	claims := &awsv1alpha1.AWSAccountClaimList{}
 
 	err := h.Store().Client().List(ctx,
 		store.ListOptions.InAllNamespaces(),
@@ -106,8 +123,8 @@ func (h awsocl) Delete(ctx context.Context, name string) (*aws.AWSOrganization, 
 }
 
 // Get returns the class from the kore
-func (h awsocl) Get(ctx context.Context, name string) (*aws.AWSOrganization, error) {
-	org := &aws.AWSOrganization{}
+func (h awsocl) Get(ctx context.Context, name string) (*awsv1alpha1.AWSOrganization, error) {
+	org := &awsv1alpha1.AWSOrganization{}
 
 	if found, err := h.Has(ctx, name); err != nil {
 		return nil, err
@@ -123,8 +140,8 @@ func (h awsocl) Get(ctx context.Context, name string) (*aws.AWSOrganization, err
 }
 
 // List returns a list of orgs
-func (h awsocl) List(ctx context.Context) (*aws.AWSOrganizationList, error) {
-	orgs := &aws.AWSOrganizationList{}
+func (h awsocl) List(ctx context.Context) (*awsv1alpha1.AWSOrganizationList, error) {
+	orgs := &awsv1alpha1.AWSOrganizationList{}
 
 	return orgs, h.Store().Client().List(ctx,
 		store.ListOptions.InNamespace(h.team),
@@ -136,7 +153,57 @@ func (h awsocl) List(ctx context.Context) (*aws.AWSOrganizationList, error) {
 func (h awsocl) Has(ctx context.Context, name string) (bool, error) {
 	return h.Store().Client().Has(ctx,
 		store.HasOptions.InNamespace(h.team),
-		store.HasOptions.From(&aws.AWSOrganization{}),
+		store.HasOptions.From(&awsv1alpha1.AWSOrganization{}),
 		store.HasOptions.WithName(name),
 	)
+}
+
+//GetAccGetAccountOUs will get any applicable OU's for Accounts given an awskey, awskeyid, region and roleArn
+func (h awsocl) GetAccountOUs(ctx context.Context, p OUParams) ([]string, error) {
+	ous := []string{}
+
+	s := awsutils.AssumeRoleFromCreds(
+		awsutils.Credentials{
+			AccessKeyID:     p.AWSKeyID,
+			SecretAccessKey: p.AWSKey,
+		},
+		p.RoleARN,
+		p.Region,
+		p.Region,
+	)
+	// Verify auth first:
+	_, err := s.Config.Credentials.Get()
+	if err != nil {
+		log.Debugf("unable to use aws credential id %s and assume role %s", p.AWSKeyID, p.RoleARN)
+
+		return ous, ErrUnauthorized
+	}
+	roots, err := awsutils.GetRoots(s)
+	if err != nil {
+
+		return ous, err
+	}
+	l := len(roots)
+	if l != 1 {
+
+		return ous, fmt.Errorf("can only support one organisational root but found %d", l)
+	}
+	rootID := aws.StringValue(roots[0].Id)
+	awsous, err := awsutils.ListOUsFromParent(s, rootID)
+	if err != nil {
+
+		return ous, err
+	}
+	for _, ou := range awsous {
+		switch ouName := aws.StringValue(ou.Name); ouName {
+		case "Root":
+			continue
+		case "Core":
+			continue
+		default:
+			ous = append(ous, ouName)
+		}
+	}
+
+	return ous, nil
 }
