16421f6d6aa6a7785d0dcdac1951e7e4792a207c
Rohith <gambol99@gmail.com>
2020-07-30 18:43:04 +0100

    - adding a lookup into maps

---
+++ pkg/utils/maps.go
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2020 Appvia Ltd <info@appvia.io>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	// ErrMapLookupNotFound indicates the value was not found
+	ErrMapLookupNotFound = errors.New("value not found")
+)
+
+// MapLookup is capable of lookup up a value in a nested map
+func MapLookup(m map[string]interface{}, ks ...string) (rval interface{}, err error) {
+	var ok bool
+
+	if len(ks) == 0 { // degenerate input
+		return nil, fmt.Errorf("NestedMapLookup needs at least one key")
+	}
+	if rval, ok = m[ks[0]]; !ok {
+		return nil, ErrMapLookupNotFound
+	} else if len(ks) == 1 { // we've reached the final key
+		return rval, nil
+	} else if m, ok = rval.(map[string]interface{}); !ok {
+		return nil, fmt.Errorf("malformed structure at %#v", rval)
+	} else { // 1+ more keys
+		return MapLookup(m, ks[1:]...)
+	}
+}
